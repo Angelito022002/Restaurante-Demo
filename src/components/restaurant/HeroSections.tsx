7c16231b6086312ca0c39ce787b9faf715c9019e
Angelito022002 <165395950+Angelito022002@users.noreply.github.com>
2026-04-16 22:17:36 -0400

    Update HeroSections.tsx

--- src/components/restaurant/HeroSections.tsx
+++ src/components/restaurant/HeroSections.tsx
@@ -6,7 +6,7 @@ import { PlaceHolderImages } from "@/lib/placeholder-images";
 
 const CATEGORIES = [
   { id: "appetizers", title: "ENTRADAS", img: "https://i.imgur.com/ZCGGJrj.jpeg" },
-  { id: "mains", title: "PLATOS FUERTES", img: "https://i.imgur.com/xeojJPQ.jpeg" },
+  { id: "mains", title: "PLATOS FUERTES", img: "https://i.imgur.com/T0n6BfC.png" },
   { id: "desserts", title: "POSTRES", img: "https://i.imgur.com/JzjaYdT.jpeg" },
 ];
 
